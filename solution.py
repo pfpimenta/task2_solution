3b5a7a48fd5a57509ffbfa3a8ef6abcaf6944807
pfpimenta <pedrofolettopimenta@gmail.com>
2020-06-25 20:09:02 -0300

    pre&post processing, example use case, refactoring

--- solution.py
+++ solution.py
@@ -7,10 +7,13 @@
 # reformulated as a supervised machine learning classification problem
 #########
 
+### imports
 import json
 import numpy as np
+from random import randint
 from sklearn.neural_network import MLPClassifier
 
+### defines
 DATASET_PATH = 'dataset/'
 NUM_DATASET_FILES = 15
 
@@ -18,6 +21,9 @@ NUM_DATASET_FILES = 15
 TRAIN_RATIO = 80 
 TEST_RATIO = 20
 #VAL_RATIO = 8 # automatically separated by scikit-learn
+
+
+### functions
 
 def load_topology_data():
 	# load graph topology
@@ -65,11 +71,10 @@ def dataset_split(dataX, dataY):
 	num_samples = len(dataX)
 	print(f'total number of samples : {num_samples}') # debug
 
-	# train/test/val set sizes
+	# train/test set sizes
 	train_size = int(num_samples*TRAIN_RATIO/100)
 	test_size = num_samples - train_size # int(num_samples*TEST_RATIO/100)
-	#val_size = num_samples - train_size - test_size #int(num_samples*VAL_RATIO/100)
-	print(f'train : {train_size}, test : {test_size}')#, val : {val_size}') # debug
+	print(f'train : {train_size}, test : {test_size}')
 
 	# do the split
 	trainX = dataX[0:train_size]
@@ -77,7 +82,7 @@ def dataset_split(dataX, dataY):
 	testX = dataX[train_size:train_size+test_size]
 	testY = dataY[train_size:train_size+test_size]
 	
-	return trainX, trainY, testX, testY#, valX, valY
+	return trainX, trainY, testX, testY
 
 def print_dataset_debug(dataX, dataY):
 	# function for debugging
@@ -102,6 +107,104 @@ def print_dataset_debug(dataX, dataY):
 	# for i in range(int(num_total_samples/2)-num_show_samples, int(num_total_samples/2)):
 	# 	print(f'dataX[{i}] : {dataX[i]} --- dataY[{i}] : {dataY[i]}')
 
+def print_topology_debug(topology_data):
+	# function for debugging
+
+	for key in topology_data.keys():
+		print(f'\n {key}:')
+		# TODO
+		###########################
+
+def readable_request(src_node, dst_node, connection_volume=None, connection_duration=None):
+	# returns a readable version of a request
+	readable_string = f"connection request from node {int(src_node)} to node {int(dst_node)}"
+	if(connection_volume != None and connection_duration != None):
+		readable_string += f" with connection volume {connection_volume} and duration {connection_duration}"
+	return readable_string
+
+def sd_node_id_2_bin_array(sd_nodes, num_nodes):
+	# this function receives:
+	# - num_nodes : the number of nodes in the graph
+	# - src_node_id, dst_node_id: IDs of the src and dst nodes
+	# and returns a binary array with 1s in the corresponding positions
+	num_samples = len(sd_nodes)
+	sd_bin_array = np.zeros((num_samples, num_nodes))
+	for i in range(num_samples):
+		src_node_id = int(sd_nodes[i, 0])
+		dst_node_id = int(sd_nodes[i, 1])
+		sd_bin_array[i, src_node_id] = 1
+		sd_bin_array[i, dst_node_id] = 1
+	
+	return sd_bin_array
+
+def data_pre_processing(dataX, num_nodes):
+	# this functions takes input samples,
+	# changes the representation of the src and dst
+	# nodes information,
+	# and removes its unnecessary features:
+	# - connection volume (always equal to 1)
+	# - connection total duration (irrelevant to the problem)
+
+	# get graph state
+	graph_state = dataX[:,0:40]
+	# get src-dst nodes information
+	sd_nodes = dataX[:,40:42]
+	sd_bin_array = sd_node_id_2_bin_array(sd_nodes,num_nodes)
+	# concatenate graph state and src-dst information
+	dataX = np.concatenate((graph_state, sd_bin_array), axis=1)
+
+	return dataX
+
+def s_d_ids_2_sd_id(src_node_id, dst_node_id, num_nodes):
+	# this function receives a source node id
+	# and a destination node id
+	# and returns the id corresponding to
+	# the path src->dst
+
+	sd_id = int((src_node_id+1)*(num_nodes-1) + dst_node_id - num_nodes + int(bool(src_node_id>dst_node_id)))
+	return sd_id
+
+def get_path(label, src_node_id, dst_node_id, path_list, num_nodes):
+	# this function receives a label indicating
+	# which of the 3 best paths (or none) was chosen
+	# for a connection request,
+	# the connection request source and destination nodes,
+	# and the total number of nodes in the graph,
+	# and then returns the list of nodes of the following path
+
+	sd_id = s_d_ids_2_sd_id(src_node_id, dst_node_id, num_nodes)
+	chosen_path_idx = int(np.argmax(label)) - 1
+	if(chosen_path_idx == -1):
+		# there is NO feasible path
+		return "<there is no feasible path>"
+	else:
+		# there IS a feasible path
+		path = path_list[sd_id][chosen_path_idx]
+		return path
+
+def post_processing(label):
+	# this functions receives a predicted label,
+	# corrects it if it is invalid,
+	# and then returns it
+
+	if(sum(label) != 1):
+		# if invalid we say that there is no feasible path
+		label = np.array([1, 0, 0, 0])
+	return label
+
+def classify_request(model, request_sample):
+	# this function runs the received prediction model
+	# with the received request_sample,
+	# passes it through the post_processing,
+	# and returns the predicted label
+
+	# prediction
+	predicted_label = model.predict([request_sample])[0]
+	# post processing
+	predicted_label = post_processing(predicted_label)
+
+	return predicted_label
+
 def test_model(model, testX, testY):
 	# calculates a score based on a comparison
 	# between the predictions and the ground truth
@@ -110,17 +213,16 @@ def test_model(model, testX, testY):
 	num_total_samples = len(testX)
 
 	# testing
-	print("\ntesting...")
 	errors = []
 	for i in range(num_total_samples):
-		pred = model.predict([testX[i]])
+		#pred = model.predict([testX[i]])
+		pred = classify_request(model, testX[i])
 		loss = np.sum(abs(pred - testY[i])) # TODO verificar se eh realmente uma 'loss'
 		errors.append(loss)
 
 		# debug : verify the wrong prediction cases
 		if(loss > 0):
-			print(f'example {i} - predicted: {pred}, ground truth: {testY[i]}') # debug
-			print(f'loss : {loss}') # debug
+			print(f'PREDICTION ERROR: example {i} - predicted: {pred}, ground truth: {testY[i]}, ---> loss : {loss}') # debug
 			
 
 
@@ -128,31 +230,8 @@ def test_model(model, testX, testY):
 	
 	return score
 
-def readable_request(src_node, dst_node, connection_volume, connection_duration):
-	# returns a readable version of a request
-	readable_string = f"connection request from node {src_node} to node {dst_node}"
-	readable_string += f" with connection volume {connection_volume} and duration {connection_duration}"
-	return readable_string
-
-def data_pre_processing(dataX):
-	# this functions takes input samples
-	# and removes its unnecessary features:
-	# - connection volume (always equal to 1)
-	# - connection total duration (irrelevant to the problem)
-	return dataX[:,0:42]
-
-
-# TODO
-def post_processing():
-	pass
-
-# TODOTODOTODOTOOTOD
-def classify_request(request):
-	# this
-	pass
-
-
-#############################3
+#############################
+### "main"
 
 if(__name__ == "__main__"):
 
@@ -163,27 +242,50 @@ if(__name__ == "__main__"):
 	dataX, dataY = load_dataset()
 
 	# preprocessing
-	dataX = data_pre_processing(dataX)
+	#dataX = data_pre_processing(dataX)
+	num_nodes = len(topology_data['adjMat'])
+	pp_dataX = data_pre_processing(dataX, num_nodes)
 
 	# print data stats and metainfo
-	#print_dataset_debug(dataX, dataY)
+	#print_dataset_debug(pp_dataX, dataY)
+	# print graph topology stats and metainfo
+	#print_topology_debug(topology_data)
 
-	### SPLIT DATASET : (training/test/validation)
-	#trainX, trainY, testX, testY, valX, valY = dataset_split(dataX, dataY)
-	trainX, trainY, testX, testY = dataset_split(dataX, dataY)
+	### SPLIT DATASET : (training/test)
+	trainX, trainY, testX, testY = dataset_split(pp_dataX, dataY)
 
 	### TRAINING
 	# create model
 	model = MLPClassifier(solver='adam', alpha=1e-5, hidden_layer_sizes=(15, 10, 5), verbose=True, early_stopping=True, n_iter_no_change=20)
-
 	print("\ntraining...")
 	# fit to data (training/learning)
 	model.fit(trainX, trainY)
+	print("...training complete")
 
 	### TESTING
-	#score = test_model(model, testX, testY)
-	score = model.score(testX, testY)
-	print(f'score: {score}')
+	print("\ntesting...")
+	score = test_model(model, testX, testY)
+	#score = model.score(testX, testY)
+	print(f'... testing score: {score}')
 
-	################################################################# TODOTODOTODOTOOTOD
-	# example use
+
+	#################################################################
+	#### EXAMPLE USE CASE
+
+	# chosing a random sample
+	random_example_index = randint(0, len(dataX))
+	sample = dataX[random_example_index]
+	expected_label = dataY[random_example_index]
+	request = readable_request(sample[40], sample[41], sample[42], sample[43])
+	# running the prediction on the chosen sample
+	predicted_label = model.predict([pp_dataX[random_example_index]])[0]
+	predicted_label = classify_request(model, pp_dataX[random_example_index])
+	# getting the paths represented by the predicted and expected labels
+	predicted_path = get_path(predicted_label, sample[40], sample[41], topology_data['list_paths'], num_nodes)
+	expected_path = get_path(expected_label, sample[40], sample[41], topology_data['list_paths'], num_nodes)
+
+
+	print(f'\n\n...random example:')
+	print(f'*Input: {request} \n      and graph state={sample[0:40]}')
+	print(f'*Predicted output: {predicted_label}\t-> path: {predicted_path} ')
+	print(f'*Expected output: {expected_label}\t-> path: {expected_path} ')
